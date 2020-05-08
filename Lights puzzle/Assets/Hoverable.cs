8d5bcb627ded6300bdd3d645d915da202ec2f782
stlows <vincent_beaulieu@live.ca>
2020-05-08 18:11:02 -0400

    initial commit

---
+++ Lights puzzle/Assets/Hoverable.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hoverable : MonoBehaviour {
+
+    public Material defaultMaterial;
+    public Material hoveredMaterial;
+    public bool isHoverable;
+}
---
+++ Lights puzzle/Assets/SelectionManager.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectionManager : MonoBehaviour {
+
+    private GameObject _selection;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit))
+        {
+            var selection = hit.transform.gameObject;
+            var selectionHoverable = selection.GetComponent<Hoverable>();
+            if (selectionHoverable != null)
+            {
+                if (selectionHoverable.isHoverable)
+                {
+                    if (selection != _selection)
+                    {
+                        ResetSelectionRender();
+                        _selection = selection;
+                        SetSelectionRender();
+                    }
+                    return;
+                }
+            }
+            
+
+        }
+
+        ResetSelectionRender();
+        _selection = null;
+
+    }
+
+    private void ResetSelectionRender()
+    {
+        if (_selection != null)
+        {
+            _selection.GetComponent<Renderer>().material = _selection.GetComponent<Hoverable>().defaultMaterial;
+        }
+    }
+
+    private void SetSelectionRender()
+    {
+        if (_selection != null)
+        {
+            _selection.GetComponent<Renderer>().material = _selection.GetComponent<Hoverable>().hoveredMaterial;
+        }
+    }
+}
