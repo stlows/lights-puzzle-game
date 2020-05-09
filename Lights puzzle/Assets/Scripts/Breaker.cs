4f34af09af9b197c7b04dddb2978aad57dd61739
stlows <vincent_beaulieu@live.ca>
2020-05-09 13:06:48 -0400

    Breaker prefab

---
+++ Lights puzzle/Assets/Scripts/Breaker.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Boo.Lang.Environments;
+using UnityEngine;
+
+public class Breaker : MonoBehaviour
+{
+
+    public Light associatedLight;
+    public float openIntensity = 10f;
+    public float closeIntensity = 0f;
+    public bool isOpened = false;
+
+    public MinimumDistance minimumDistance;
+
+    private Transform arm;
+    private Vector3 closedAngle = new Vector3(-30, 0, 0);
+    private Vector3 openedAngle = new Vector3(30, 0, 0);
+
+    // Use this for initialization
+    void Start ()
+    {
+        arm = transform.Find("ArmWrapper");
+        if (isOpened)
+        {
+            Open();
+        }
+        else
+        {
+            Close();
+        }
+    }
+	
+    void OnMouseUp()
+    {
+        if (!minimumDistance.Check())
+        {
+            return;
+        }
+
+        if (isOpened)
+        {
+            Close();
+        }
+        else
+        {
+            Open();
+        }
+        gameObject.GetComponent<AudioSource>().Play();
+        isOpened = !isOpened;
+    }
+
+    void Close()
+    {
+        arm.localEulerAngles = closedAngle;
+        associatedLight.intensity = closeIntensity;
+    }
+
+    void Open()
+    {
+        arm.localEulerAngles = openedAngle;
+        associatedLight.intensity = openIntensity;
+    }
+}
---
+++ Lights puzzle/Assets/Scripts/MinimumDistance.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MinimumDistance : MonoBehaviour
+{
+    public float minimumDistance;
+    public Transform player;
+
+    public bool Check()
+    {
+        return Vector3.Distance(player.position, transform.position) < minimumDistance;
+
+    }
+}
