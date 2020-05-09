0a5352b82ae018fecd341d6d374bb2c5902a2699
stlows <vincent_beaulieu@live.ca>
2020-05-08 21:30:42 -0400

    structure des fichiers

---
+++ Lights puzzle/Assets/Scripts/OpenDoor.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenDoor : MonoBehaviour
+{
+
+    [SerializeField]
+    private Animator openDoorController;
+
+    public Transform player;
+
+    public float minimalDistanceToSwitch;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnMouseDown()
+    {
+        if (Vector3.Distance(player.position, transform.position) > minimalDistanceToSwitch)
+        {
+            return;
+        }
+        if (openDoorController.GetBool("opened"))
+        {
+            return;
+        }
+        else
+        {
+            GameObject.FindGameObjectWithTag("SuccessSound").GetComponent<AudioSource>().Play();
+            openDoorController.SetBool("opened", true);
+        }
+    }
+}
