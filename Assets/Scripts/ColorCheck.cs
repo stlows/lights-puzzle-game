323146ae0e4a2cb58c7fd519f26292523b294a82
Laurent Bergeron <laurent.bergeron4@gmail.com>
2020-05-13 12:33:35 -0700

    a second camera tracks the color under the player

---
+++ Assets/Scripts/ColorCheck.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using System;
+using System.Linq;
+using UnityEngine;
+
+public class ColorCheck : MonoBehaviour
+{
+    public RenderTexture colorCheckTexture;
+    public Color groundColor;
+    public PowerColor powerColor;
+    private Texture2D tex;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    double euclidianDistance(Color color1, Color color2)
+    {
+        double rDiff = color1.r - color2.r;
+        double gDiff = color1.g - color2.g;
+        double bDiff = color1.b - color2.b;
+        // https://stackoverflow.com/questions/1847092/given-an-rgb-value-what-would-be-the-best-way-to-find-the-closest-match-in-the-d
+        // Future improvement: use weighted approach?
+        return Math.Sqrt((rDiff * rDiff) + (gDiff * gDiff) + (bDiff * bDiff));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Remember currently active render texture
+        RenderTexture currentActiveRT = RenderTexture.active;
+
+        // Set the supplied RenderTexture as the active one
+        RenderTexture.active = colorCheckTexture;
+
+        // Create a new Texture2D and read the RenderTexture image into it
+        tex = new Texture2D(colorCheckTexture.width, colorCheckTexture.height);
+        tex.ReadPixels(new Rect(0, 0, tex.width, tex.height), 0, 0);
+
+        // Restore previously active render texture
+        RenderTexture.active = currentActiveRT;
+
+        // Read Color Pixel from texture 2D
+        groundColor = tex.GetPixel(0, 0);
+
+        Color[] colorArray = { 
+            Color.white, 
+            Color.black,
+            Color.red,
+            Color.green,
+            Color.blue,
+            Color.cyan,
+            Color.magenta,
+            Color.yellow
+        };
+        double[] distanceArray = new double[8];
+
+
+        for (int i=0; i<8; i++)
+        {
+            distanceArray[i] = euclidianDistance(groundColor, colorArray[i]);
+        }
+
+        powerColor = (PowerColor) Array.IndexOf(distanceArray, distanceArray.Min());
+    }
+}
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -10,18 +10,26 @@ public class PlayerMovement : MonoBehaviour
     public float gravity = -100f;
     public float jumpSpeed = 35f;
     public LayerMask groundMask;    
-
-    private Vector3 velocity;
+	
+	
+	private PowerColor groundColorLabel;
+	private Vector3 velocity;
 	private float groundDistance = 0.1f;
 	private bool isGrounded;
+
 
 	// Update is called once per frame
 	void Update ()
 	{
+		// Update color the player is stading on (reads from ColorCheck.cs)
+		groundColorLabel = gameObject.GetComponent<ColorCheck>().powerColor;
+
 		// Lateral mouvement
-	    float x = Input.GetAxis("Horizontal");
+		float x = Input.GetAxis("Horizontal");
 	    float z = Input.GetAxis("Vertical");
         Vector3 move = transform.right * x + transform.forward * z;
+		move = (groundColorLabel == PowerColor.GREEN) ? move * speed * 3 : move * speed;
+		//move = (groundColorLabel == PowerColor.BLACK) ? -move : move;
 
 		// Jump
 		Vector3 sphere_position = transform.position + Vector3.down * (controller.height * .5f - controller.radius);
@@ -33,12 +41,12 @@ public class PlayerMovement : MonoBehaviour
 		}
 		if (Input.GetButtonDown("Jump") && isGrounded)
 	    {
-			velocity.y = jumpSpeed;
+			velocity.y = (groundColorLabel == PowerColor.RED) ? jumpSpeed * 3 : jumpSpeed;
 		}
 	    velocity.y += gravity * Time.deltaTime;
 
 		// Update controller
-		controller.Move(move * speed * Time.deltaTime);
+		controller.Move(move * Time.deltaTime);
 		controller.Move(velocity * Time.deltaTime);
 	}
 }
