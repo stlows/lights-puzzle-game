aa0cf194c558656a4d0a16ea549a2b161fa047dc
Laurent Bergeron <laurent.bergeron4@gmail.com>
2020-05-21 09:23:38 -0700

    class inheritance for breakers and timers

---
+++ Assets/Scripts/Breaker/BreakerColor.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Boo.Lang.Environments;
+using UnityEngine;
+ 
+public class BreakerColor : Breaker
+{
+    public Color openColor = Color.white;
+    public Color closeColor = Color.black;
+    public override void CloseSpecific()
+    {
+        foreach (Light light in associatedLights)
+        {
+            light.color = closeColor;
+        }
+    }
+
+    public override void OpenSpecific()
+    {
+        foreach (Light light in associatedLights)
+        {
+            light.color = openColor;
+        }
+    }
+}
---
+++ Assets/Scripts/Breaker/BreakerIntensity.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Boo.Lang.Environments;
+using UnityEngine;
+ 
+public class BreakerIntensity : Breaker
+{
+    public float openIntensity = 2f;
+    public float closeIntensity = 0f;
+    public override void CloseSpecific()
+    {
+        foreach (Light light in associatedLights)
+        {
+            light.intensity = closeIntensity;
+        }
+    }
+
+
+    public override void OpenSpecific()
+    {
+        foreach (Light light in associatedLights)
+        {
+            light.intensity = openIntensity;
+        }
+    }
+}
