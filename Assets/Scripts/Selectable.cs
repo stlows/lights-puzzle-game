b9da5a929347071d45a1100dba40f60997f7c1d7
Laurent Bergeron <laurent.bergeron4@gmail.com>
2020-11-01 11:24:18 -0800

    simplified SelectableAsButton to Selectable, only need a GetKeyUp event to trigger

--- Assets/Scripts/Selectable.cs
+++ Assets/Scripts/Selectable.cs
@@ -1,49 +1,19 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public abstract class Selectable : MonoBehaviour
 {
     public float minimumDistance;
     private const KeyCode triggerKey = KeyCode.Mouse0;
-    private bool keyCodeWasDown;
     private RaycastHit hitInfo;
-
-    void Start()
-    {
-        keyCodeWasDown = false;
-    }
 
     protected bool ButtonIsActivated()
     {
-        // On a keyDown event, check for hit.
-        if (Input.GetKeyDown(triggerKey))
+        if (Input.GetKeyUp(triggerKey))
         {
             if (CheckRaycastHit())
             {
-                keyCodeWasDown = true;
-                return false;
-            }
-        }
-
-        // If the keyDown was a successful hit and the key remained pressed, check for hit on keyUp.
-        if (keyCodeWasDown && Input.GetKeyUp(triggerKey))
-        {
-            if (CheckRaycastHit())
-            {
-                keyCodeWasDown = false;
                 return true;
             }
-        }
-
-        // If the keyDown was a succesful hit, maintain success as long as the key stays pressed
-        if (keyCodeWasDown && Input.GetKey(triggerKey))
-        {
-            keyCodeWasDown = true;
-        }
-        else
-        {
-            keyCodeWasDown = false;
         }
         return false;
     }
