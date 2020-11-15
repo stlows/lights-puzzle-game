9657ab0039cd4c25ed91f19922425c90477a6360
Laurent Bergeron <laurent.bergeron4@gmail.com>
2020-11-15 12:19:27 -0700

    updated to unity 2019

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,8 +1,5 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using System;
+﻿using System;
 using UnityEngine;
-using UnityEngine.Experimental.UIElements;
 
 public class PlayerMovement : MonoBehaviour
 {
