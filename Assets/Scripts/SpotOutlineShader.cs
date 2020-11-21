d151a8141981acbc1f17da77301b528d47ad885a
Laurent Bergeron <laurent.bergeron4@gmail.com>
2020-11-21 01:54:06 -0700

    dynamic spotlight outline. cool stuff

--- Assets/Scripts/SpotOutlineShader.cs
+++ Assets/Scripts/SpotOutlineShader.cs
@@ -5,16 +5,56 @@ using UnityEngine;
 public class SpotOutlineShader : MonoBehaviour
 {
 
-    private GameObject cylinder;
-    private GameObject cube;
-    private GameObject box;
+    private Material cylinderMat;
+    private Material cubeMat;
+    private Material boxMat;
+
+    private Light light;
+    private Color currentColor;
+    private float currentIntensity;
+
     void Start()
     {
-        //cylinder = gameObject.Find
+        light = transform.Find("Rotating/Light").GetComponent<Light>();
+        currentColor = light.color;
+        currentIntensity = light.intensity;
+
+        cylinderMat = transform.Find("Base/Cylinder").GetComponent<Renderer>().material;
+        cubeMat = transform.Find("Base/Cube").GetComponent<Renderer>().material;
+        boxMat = transform.Find("Rotating/Box").GetComponent<Renderer>().material;
+
+        UpdateOutlineColor(currentColor);
+        UpdateOutlineWidth(currentIntensity);
     }
 
     void Update()
     {
-        
+        Color newColor = light.color; 
+        if (newColor != currentColor)
+        {
+            UpdateOutlineColor(newColor);
+        }
+        currentColor = newColor;
+
+        float newIntensity = light.intensity;
+        if (newIntensity != currentIntensity)
+        {
+            UpdateOutlineWidth(newIntensity);
+        }
+        currentIntensity = newIntensity;
+    }
+
+    private void UpdateOutlineWidth(float newIntensity)
+    {
+        boxMat.SetFloat("_FirstOutlineWidth", (newIntensity > 0) ? 0.05f : 0f);
+        cubeMat.SetFloat("_FirstOutlineWidth", (newIntensity > 0) ? 0.05f : 0f);
+        cylinderMat.SetFloat("_FirstOutlineWidth", (newIntensity > 0) ? 0.15f : 0f);
+    }
+
+    private void UpdateOutlineColor(Color newColor)
+    {
+        boxMat.SetVector("_FirstOutlineColor", newColor);
+        cubeMat.SetVector("_FirstOutlineColor", newColor);
+        cylinderMat.SetVector("_FirstOutlineColor", newColor);
     }
 }
